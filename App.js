918253fdcfc52fcda99faf05193b46e755d7fa9f
capmec <capmec@gmail.com>
2024-07-19 22:53:51 +0200

    5.1: Building Native Applications with JavaScript

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
 // import the screens
 import Start from './components/Start'
 import Chat from './components/Chat'
@@ -5,7 +8,6 @@ import Chat from './components/Chat'
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-// import { StyleSheet, Text, View } from 'react-native';
 
 // Create the navigator
 const Stack = createNativeStackNavigator()
@@ -20,14 +22,5 @@ const App = () => {
     </NavigationContainer>
   )
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
 
 export default App
